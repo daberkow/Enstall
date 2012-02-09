f765ae351b2156e74e25ca82c3e03727562b370a
Dan Berkowitz <dansberkowitz@gmail.com>
2012-02-09 12:10:05 -0500

    This is a temp web page, with the most basic layout and a single package, this will allow the client to start being worked on

---
+++ web/packages.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./style.css"/>
+		<script src="../jquery-1.6.2.min.js"></script> <!--Only used for easy ajax requests-->
+		
+	</head>
+	<body>
+		<div id="main">
+			<div id="row">
+				<span class="Title">Super Awesome Mega Package 9000</span>
+				<a href="./package.xml"><div id="Install">Install</div></a>
+			</div>
+		</div>
+	</body>
+</html>
